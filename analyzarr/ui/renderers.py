9e22186742f90e793f308f6c9486224486a7264d
Michael Sarahan <msarahan@gmail.com>
2013-01-26 10:46:02 -0800

    moved plot rendering out of class, fixed several bugs in scatter plot code

--- analyzarr/ui/renderers.py
+++ analyzarr/ui/renderers.py
@@ -11,6 +11,122 @@ from chaco.tools.cursor_tool import CursorTool, BaseCursorTool
 
 from chaco.data_range_1d import DataRange1D
 
+import numpy as np
+
+def _render_plot(self, array_plot_data):
+    """
+    data is a numpy array to be plotted as a line plot.
+    The first column of the data is used as x, the second is used as y.
+    """
+    plot = Plot(array_plot_data)
+    plot.plot(("x","y"), type="line", name="base_plot")[0]
+
+    # attach the pan and zoom tools
+    plot.tools.append(PanTool(plot,drag_button="right"))
+    zoom = ZoomTool(plot, tool_mode="box", always_on=False, aspect_ratio=plot.aspect_ratio)
+    plot.overlays.append(zoom)
+    return plot
+
+def _render_image(array_plot_data, title=None):
+    plot = Plot(array_plot_data,default_origin="top left")        
+    plot.img_plot("imagedata", colormap=gray, name="base_plot")
+    # todo: generalize title and aspect ratio
+    plot.title = title
+    data_array = array_plot_data.arrays['imagedata']
+    plot.aspect_ratio=float(data_array.shape[1]) / float(data_array.shape[0])
+    # attach the rectangle tool
+    plot.tools.append(PanTool(plot,drag_button="right"))
+    zoom = ZoomTool(plot, tool_mode="box", always_on=False, aspect_ratio=plot.aspect_ratio)
+    plot.overlays.append(zoom)
+    return plot
+
+def _render_scatter_overlay(base_plot, array_plot_data,
+                            marker="circle", fill_alpha=0.5,
+                            marker_size=6):
+    if 'index' not in array_plot_data.arrays:
+        return base_plot, None
+    # the simple case - no color data
+    if "color" not in array_plot_data.arrays:
+        scatter_plot = base_plot.plot(("index", "value"),
+                      type="scatter",
+                      name="scatter_plot",
+                      marker = marker,
+                      fill_alpha = fill_alpha,
+                      marker_size = marker_size,
+                      )
+        colorbar = None
+    # slightly more involved: colors mapped to some value
+    # with a threshold control that links to transparency
+    else:
+        color_data = array_plot_data.get_data('color')
+        scatter_plot = base_plot.plot(("index", "value", "color"),
+                      type="cmap_scatter",
+                      name="scatter_plot",
+                      color_mapper=jet(DataRange1D(low = 0.0,
+                                       high = 1.0)),                      
+                      marker = marker,
+                      fill_alpha = fill_alpha,
+                      marker_size = marker_size,
+                      )
+        colorbar = ColorBar(index_mapper = LinearMapper(
+            range = DataRange1D(
+                low = np.min(color_data),
+                high = np.max(color_data),
+                )
+            ),
+            orientation = 'v',
+            resizable = 'v',
+            width = 30,
+            )
+        # this part is for making the colormapped points fade when they
+        #  are not selected by the threshold.
+        # The renderer is the actual class that does the rendering - 
+        # the Plot class calls this other class.  When we say 
+        #   plots['scatter_plot']
+        # we are getting the named plot that we created above.
+        # The extra [0] on the end is because we get a list - in case
+        # there is more than one plot named "scatter_plot"
+        scatter_renderer = scatter_plot[0]
+        selection = ColormappedSelectionOverlay(scatter_renderer, 
+                                                fade_alpha=0.35, 
+                                                selection_type="range")
+        scatter_renderer.overlays.append(selection)
+        #if self.threshold is not None:
+        #    scatter_renderer.color_data.metadata['selections']=self.threshold
+        #    scatter_renderer.color_data.metadata_changed={'selections':self.threshold}
+        colorbar_selection=RangeSelection(component=colorbar)
+        colorbar.tools.append(colorbar_selection)
+        colorbar.overlays.append(RangeSelectionOverlay(component=colorbar,
+                                          border_color="white",
+                                          alpha=0.8,
+                                          fill_color="lightgray",
+                                          metadata_name='selections',
+                                          )
+                                 ) 
+        colorbar.padding_top = scatter_renderer.padding_top
+        colorbar.padding_bottom = scatter_renderer.padding_bottom
+        #self._colorbar_selection = colorbar_selection
+    #scatter_plot.x_grid.visible = False
+    #scatter_plot.y_grid.visible = False
+    #scatter_plot.range2d = base_plot.range2d
+    #self._scatter_plot = scatter_plot
+    return base_plot, colorbar
+
+def _render_quiver_overlay(base_plot, array_plot_data, 
+                           line_color="white", line_width=1.0, 
+                           arrow_size=5):
+    if 'index' not in array_plot_data.arrays:
+        return Plot()
+    plot = Plot(array_plot_data, aspect_ratio = base_plot.aspect_ratio, 
+                default_origin="top left")
+    plot.quiverplot(("index", "value", "vectors"), name="quiver_plot",
+                    line_color=line_color, line_width=line_width, 
+                    arrow_size=arrow_size)
+    quiverplot.x_grid.visible = False
+    quiverplot.y_grid.visible = False
+    quiverplot.range2d = base_plot.range2d
+    return quiverplot
+
 class HasRenderer(HasTraits):
     _quiver_scale = Float(1.0)
     _scatter_threshold = Trait(None,None,List,Tuple,Array)
@@ -24,152 +140,50 @@ class HasRenderer(HasTraits):
         # container isn't necessary here, but we do it to keep it consistent
         #   with how the other plot types return data.        
         plot_container = OverlayPlotContainer(plot)
+        self._base_plot = plot
+        self.plot_container = plot_container
         return plot_container
 
     def get_simple_image_plot(self, array_plot_data, title=None):
-        image_plot = self._render_image(array_plot_data=array_plot_data, 
+        image_plot = _render_image(array_plot_data=array_plot_data, 
                                         title=title)
         # container isn't necessary here, but we do it to keep it consistent
         #   with how the other plot types return data.
         image_container = OverlayPlotContainer(image_plot)
+        self._base_plot = image_plot
+        self.image_container = image_container
         return image_container
 
     # TODO - add optional appearance tweaks
     def get_scatter_overlay_plot(self, array_plot_data, title=None):
         colorbar = None
         image_plot = _render_image(array_plot_data, title)
-        scatter_plot, colorbar = _render_scatter_overlay(array_plot_data)
-        image_container = OverlayPlotContainer(image_plot, scatter_plot)
-        if colorbar is not None:
-            image_container = HPlotContainer(image_container, colorbar)
-        return image_container
+        scatter_plot, colorbar = _render_scatter_overlay(image_plot, 
+                                                         array_plot_data)
+        #image_container = OverlayPlotContainer(image_plot, scatter_plot)
+        #if colorbar is not None:
+            #image_container = HPlotContainer(image_container, colorbar)
+        self._base_plot = image_plot
+        self._scatter_plot = scatter_plot
+        #self.image_container = image_container
+        return scatter_plot
             
     # TODO - add optional appearance tweaks
     def get_scatter_quiver_plot(self, array_plot_data, title=None):
         colorbar = None
         image_plot = _render_image(array_plot_data, title)
-        scatter_plot, colorbar = _render_scatter_overlay(array_plot_data)
-        quiver_plot = _render_quiver_overlay(array_plot_data)
+        scatter_plot, colorbar = _render_scatter_overlay(image_plot,
+                                                         array_plot_data)
+        quiver_plot = _render_quiver_overlay(image_plot, array_plot_data)
         image_container = OverlayPlotContainer(image_plot, quiver_plot, 
                                                scatter_plot)
         if colorbar is not None:
             image_container = HPlotContainer(image_container, colorbar)
+        self._base_plot = image_plot
+        self._scatter_plot = scatter_plot
+        self._quiver_plot = quiver_plot
+        self.image_container = image_container
         return image_container
     
     def set_plot_title(self, title):
         self._base_plot.title=title
-    
-    def _render_plot(self, array_plot_data):
-        """
-        data is a numpy array to be plotted as a line plot.
-        The first column of the data is used as x, the second is used as y.
-        """
-        plot = Plot(array_plot_data)
-        plot.plot(("x","y"), type="line", name="base_plot")[0]
-    
-        # attach the pan and zoom tools
-        plot.tools.append(PanTool(plot,drag_button="right"))
-        zoom = ZoomTool(plot, tool_mode="box", always_on=False, aspect_ratio=plot.aspect_ratio)
-        plot.overlays.append(zoom)
-        #self._base_plot = plot.plots['base_plot'][0]
-        self._base_plot = plot
-        return plot
-
-    def _render_image(self, array_plot_data, title=None):
-        plot = Plot(array_plot_data,default_origin="top left")        
-        plot.img_plot("imagedata", colormap=gray, name="base_plot")
-        # todo: generalize title and aspect ratio
-        plot.title = title
-        data_array = array_plot_data.arrays['imagedata']
-        plot.aspect_ratio=float(data_array.shape[1]) / float(data_array.shape[0])
-        # attach the rectangle tool
-        plot.tools.append(PanTool(plot,drag_button="right"))
-        zoom = ZoomTool(plot, tool_mode="box", always_on=False, aspect_ratio=plot.aspect_ratio)
-        plot.overlays.append(zoom)
-        self._base_plot = plot
-        #self._base_plot = plot.plots['base_plot'][0]
-        return plot
-
-    def _render_scatter_overlay(self, base_plot, array_plot_data,
-                                marker="circle", fill_alpha="0.5",
-                                marker_size=6):
-        plot = Plot(image_data, aspect_ratio = base_plot.aspect_ratio, 
-                    default_origin="top left")
-        # the simple case - no color data
-        if "color" not in array_plot_data.keys():
-            scatter_plot = plot.plot(("index", "value"),
-                          type="scatter",
-                          name="scatter_plot",
-                          marker = marker,
-                          fill_alpha = fill_alpha,
-                          marker_size = marker_size,
-                          )
-            colorbar = None
-        # slightly more involved: colors mapped to some value
-        # with a threshold control that links to transparency
-        else:
-            color_data = array_plot_data.get_data('color')
-            scatter_plot = plot.plot(("index", "value", "color"),
-                          type="cmap_scatter",
-                          name="scatter_plot",
-                          marker = marker,
-                          fill_alpha = fill_alpha,
-                          marker_size = marker_size,
-                          )
-            colorbar = ColorBar(index_mapper = LinearMapper(
-                range = DataRange1D(
-                    low = np.min(color_data),
-                    high = np.max(color_data),
-                    )
-                ),
-                orientation = 'v',
-                resizable = 'v',
-                width = 30,
-                )
-            # this part is for making the colormapped points fade when they
-            #  are not selected by the threshold.
-            # The renderer is the actual class that does the rendering - 
-            # the Plot class calls this other class.  When we say 
-            #   plots['scatter_plot']
-            # we are getting the named plot that we created above.
-            # The extra [0] on the end is because we get a list - in case
-            # there is more than one plot named "scatter_plot"
-            scatter_renderer = scatter_plot.plots["scatter_plot"][0]
-            selection = ColormappedSelectionOverlay(scatter_renderer, 
-                                                    fade_alpha=0.35, 
-                                                    selection_type="range")
-            scatter_renderer.overlays.append(selection)
-            if self.threshold is not None:
-                scatter_renderer.color_data.metadata['selections']=self.threshold
-                scatter_renderer.color_data.metadata_changed={'selections':self.threshold}
-            colorbar_selection=RangeSelection(component=colorbar)
-            colorbar.tools.append(colorbar_selection)
-            colorbar.overlays.append(RangeSelectionOverlay(component=colorbar,
-                                              border_color="white",
-                                              alpha=0.8,
-                                              fill_color="lightgray",
-                                              metadata_name='selections',
-                                              )
-                                     ) 
-            colorbar.padding_top = scatter_renderer.padding_top
-            colorbar.padding_bottom = scatter_renderer.padding_bottom
-            self._colorbar_selection = colorbar_selection
-        scatter_plot.x_grid.visible = False
-        scatter_plot.y_grid.visible = False
-        scatter_plot.range2d = base_plot.range2d
-        self._scatter_plot = scatter_plot
-        return scatter_plot, colorbar
-
-    def _render_quiver_overlay(self, base_plot, array_plot_data, 
-                               line_color="white", line_width=1.0, 
-                               arrow_size=5):
-        plot = Plot(image_data, aspect_ratio = base_plot.aspect_ratio, 
-                    default_origin="top left")
-        plot.quiverplot(("index", "value", "vectors"), name="quiver_plot",
-                        line_color=line_color, line_width=line_width, 
-                        arrow_size=arrow_size)
-        quiverplot.x_grid.visible = False
-        quiverplot.y_grid.visible = False
-        quiverplot.range2d = base_plot.range2d
-        self._quiver_plot = quiverplot
-        return quiverplot
