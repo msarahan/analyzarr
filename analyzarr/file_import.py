6beebe97ed5c0cdb91510cce81de3db22e09e195
Michael Sarahan <msarahan@gmail.com>
2013-02-06 18:08:54 -0800

    Rethink file importing - will go to user creating project first (from which they will be guided to import files.)  They will be able to add files afterwards using "import files" menu

--- analyzarr/file_import.py
+++ analyzarr/file_import.py
@@ -16,11 +16,15 @@ dm_extensions = ['.dm3',]
 # I don't give a rat's ass about FEI's MRC files or other spectral formats.  
 # Sorry.
 
+def new_treasure_chest(filename):
+    h5file = get_image_h5file(output_filename)
+    return h5file
+
 def open_treasure_chest(filename):
     h5file = tb.openFile(filename, 'a')
     return h5file
 
-def import_files(file_string, output_filename = None):
+def import_files(h5file, file_string):
 
     # match supported file input types - check extension
     if '*' in file_string:
@@ -33,119 +37,96 @@ def import_files(file_string, output_filename = None):
         flist = [file_string]
 
     if os.path.splitext(flist[0])[1] in img_extensions:
-        h5file = import_image(flist, output_filename)
+        import_image(h5file, flist)
     elif os.path.splitext(flist[0])[1] in tiff_extensions:
-        h5file = import_tiff(flist, output_filename)
+        import_tiff(h5file, flist)
     elif os.path.splitext(flist[0])[1] in dm_extensions:
-        h5file = import_dm(flist, output_filename)
-    else:
-        h5file = None
+        import_dm(h5file, flist)
     h5file.flush()
-    return h5file
 
 #TODO: add ways to add/remove member data
 
-def import_image(flist, output_filename=None):
+def import_image(h5file, flist, output_filename=None):
     from scipy.misc import imread
-    if output_filename is None:
-        output_filename = "image_treasure_%s" % time.strftime("%Y%m%d_%H%M", 
-                                                             time.localtime())
-    h5file = get_image_h5file(output_filename)
     data_record = h5file.root.image_description.row
     # any kind of jpg, png can be lumped together
     for f in flist:
-        # get data as numpy array
-        d = imread(f)
-        # add a CArray for this data in the h5file
-        ds = h5file.createCArray(h5file.root.rawdata, 
-                            os.path.splitext(os.path.split(f)[1])[0],
+        filename = os.path.splitext(os.path.split(f)[1])[0]
+        if len(h5file.root.image_description.getWhereList('filename=="%s"'%filename)) == 0:
+            # get data as numpy array
+            d = imread(f)
+            # add a CArray for this data in the h5file
+            ds = h5file.createCArray(h5file.root.rawdata, 
+                            filename,
                             tb.Atom.from_dtype(d.dtype),
                             d.shape,
                             filters=filters
                             )
-        # assigns the data to the array
-        ds[:] = d
-        # add the record for this image to the table in the h5file
-        data_record['filename'] = os.path.splitext(os.path.split(f)[1])[0]
-        data_record['idx'] = flist.index(f)
-        data_record.append()
+            # assigns the data to the array
+            ds[:] = d
+            # add the record for this image to the table in the h5file
+            data_record['filename'] = filename
+            data_record['idx'] = flist.index(f)
+            data_record.append()
     h5file.root.image_description.flush()
     h5file.flush()
-    return h5file
-        
 
-def import_tiff(flist, output_filename=None):
+def import_tiff(h5file, flist):
     # for tiff, we use Christoph Gohlke's reader
     from lib.io.tifffile import imread
-    if output_filename is None:
-        output_filename = "image_treasure_%s" % time.strftime("%Y%m%d_%H%M", 
-                                                             time.localtime())   
-    h5file = get_image_h5file(output_filename)
     data_record = h5file.root.image_description.row
     # any kind of jpg, png can be lumped together
     for f in flist:
-        # get data as numpy array
-        d = imread(f)
-        # add a CArray for this data in the h5file
-        ds = h5file.createCArray(h5file.root.rawdata, 
-                            os.path.splitext(os.path.split(f)[1])[0], 
+        filename = os.path.split(f)[1])[0]
+        if len(h5file.root.image_description.getWhereList('filename=="%s"'%filename)) == 0:
+            # get data as numpy array
+            d = imread(f)
+            # add a CArray for this data in the h5file
+            ds = h5file.createCArray(h5file.root.rawdata, 
+                            filename,
                             tb.Atom.from_dtype(d.dtype),
                             d.shape,
                             filters=filters
                             )
-        # assigns the data to the array
-        ds[:] = d
+            # assigns the data to the array
+            ds[:] = d
 
-        # add the record for this image to the table in the h5file
-        data_record['filename'] = os.path.splitext(os.path.split(f)[1])[0]
-        data_record['idx'] = flist.index(f)
-        data_record.append()
+            # add the record for this image to the table in the h5file
+            data_record['filename'] = filename
+            data_record['idx'] = flist.index(f)
+            data_record.append()
     h5file.root.image_description.flush()
     h5file.flush()
-    return h5file
-        
 
 # DM3 files
-def import_dm(flist, output_filename=None):
+def import_dm(h5file, flist):
     from lib.io.digital_micrograph import file_reader
     tmp_dm3, tmp_tags = file_reader(flist[0])
     
-    if tmp_dm3.record_by is 'spectrum':
-        if output_filename is None:
-            output_filename = "spectrum_treasure_%s" % time.strftime(
-                                                        "%Y%m%d_%H%M", 
-                                                        time.localtime())
-        h5file = get_spectrum_h5file(output_filename)
-    elif tmp_dm3.record_by is 'image':
-        if output_filename is None:
-            output_filename = "image_treasure_%s" % time.strftime(
-                                                        "%Y%m%d_%H%M", 
-                                                        time.localtime())
-        h5file = get_image_h5file(output_filename)
-    
     data_record = h5file.root.image_description.row
     for f in flist:
-        print "loading file: %s" %f
-        # this is a little wasteful (re-reading the first file), 
-        # but I'm vain and want to save a line or two of code.
-        tmp_dm3, tmp_tags = file_reader(f)
-        # add a CArray for this data in the h5file
-        ds = h5file.createCArray(h5file.root.rawdata, 
-                            os.path.splitext(os.path.split(f)[1])[0], 
+        filename = os.path.splitext(os.path.split(f)[1])[0]
+        if len(h5file.root.image_description.getWhereList('filename=="%s"'%filename)) == 0:
+            print "loading file: %s" %f
+            # this is a little wasteful (re-reading the first file), 
+            # but I'm vain and want to save a line or two of code.
+            tmp_dm3, tmp_tags = file_reader(f)
+            # add a CArray for this data in the h5file
+            ds = h5file.createCArray(h5file.root.rawdata, 
+                            filename, 
                             tb.Atom.from_dtype(tmp_dm3.data.dtype),
                             tmp_dm3.data.shape,
                             filters=filters
                             )
-        # assigns the data to the array
-        ds[:] = tmp_dm3.data
+            # assigns the data to the array
+            ds[:] = tmp_dm3.data
 
-        # TODO: add the tags as metadata for the CArray
-        
-        # add the record for this image to the table in the h5file
-        data_record['filename'] = os.path.splitext(os.path.split(f)[1])[0]
-        data_record['idx'] = flist.index(f)
-        data_record.append()
+            # TODO: add the tags as metadata for the CArray
+            
+            # add the record for this image to the table in the h5file
+            data_record['filename'] = filename
+            data_record['idx'] = flist.index(f)
+            data_record.append()
     # flush the data to commit our changes to the file.
     h5file.root.image_description.flush()
     h5file.flush()
-    return h5file
