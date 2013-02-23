Several fixes required for making PCA execution and viewing function: - characterize average image for peak locations on factor images for peaks - implement data recording for peak PCA results (stored as PyTables table) - simplified and fixed view code - added controls for selecting characteristics/shifts to factor view dockpane

analyzarr/controllers/Cell.py
@@ -153,13 +153,17 @@ class CellController(BaseImageController):
                                'cell_peaks', description=desc)        
         # for each file in the cell_data group, run analysis.
         nodes = self.chest.listNodes('/cells')
-        node_names = [node.name for node in nodes if node.name not in ['template', 'average']]
+        # exclude some nodes
+        node_names = [node.name for node in nodes if node.name not in ['template']]
         for node in node_names:
-            numcells = nodes[node_names.index(node)].shape[0]
-            data = np.zeros((numcells),dtype=dtypes)
+            cell_data = nodes[node_names.index(node)][:]
+            cell_data = cell_data.reshape((-1, cell_data.shape[-2], 
+                                           cell_data.shape[-1]))            
+            data = np.zeros((cell_data.shape[0]),dtype=dtypes)
             data['filename'] = node
-            data['file_idx'] = np.arange(numcells)
-            attribs = pc.peak_attribs_stack(nodes[node_names.index(node)][:],
+            data['file_idx'] = np.arange(cell_data.shape[0])            
+            # reshape possibly 2D arrays (average fits this)
+            attribs = pc.peak_attribs_stack(cell_data,
                             peak_width=peak_width, subpixel=subpixel,
                             target_locations=target_locations,
                             target_neighborhood=target_neighborhood,
@@ -170,6 +174,7 @@ class CellController(BaseImageController):
                 data[names[name_idx]] = attribs[:, name_idx]
             # add the data to the table
             self.chest.root.cell_peaks.append(data)
+            self.chest.root.cell_peaks.flush()
         # add an attribute for the total number of peaks recorded
         self.chest.root.cell_peaks.setAttr('number_of_peaks', self.numpeaks)
         self.chest.root.cell_peaks.flush()
@@ -204,7 +209,7 @@ class CellController(BaseImageController):
         """
         if len(chars) > 0:
             if len(indices) is 0:
-                indices = range(self.chest.root.cell_peaks.number_of_peaks)
+                indices = range(self.chest.getNodeAttr('/cell_peaks','number_of_peaks'))
             # the columns we get are the combination of the chars with the
             #   indices we want.
             cols = [['%s%i' % (c, i) for i in indices] for c in chars]

analyzarr/controllers/MDA_execute.py
@@ -38,7 +38,7 @@ class MDAExecutionController(HasTraits):
             # do we have any peaks?
             try:
                 self.chest.getNode('/','cell_peaks')
-                self.has_peaks = (self.chest.root.cell_peaks.nrows > 0)
+                self.has_peaks = bool((self.chest.root.cell_peaks.nrows > 0))
             except:
                 pass
 
@@ -65,18 +65,62 @@ class MDAExecutionController(HasTraits):
             self.PCA(n_components=self.number_to_derive)
         elif method == 'ICA':
             self.ICA(n_components=self.number_to_derive)
+        self.chest.setNodeAttr('/mda_results/'+self.context, 
+                               'dimensionality', self.number_to_derive)
         # close the dialog window
         Application.instance().end_session(self._session_id)
         # show the results windows
         self.parent.update_mda_data()
 
+    def get_peak_data(self, chars=[], indices=[]):
+        """
+        Get peak data from the table of peak data.
+
+        Input:
+        chars - a list of characteristic names (as string letters).
+            For example, ['dx', 'dy', 'h'] selects x and y deviation from 
+            average peak position and the peak height.
+            
+            Possible options include:
+            x - the x position of a peak in a cell
+            y - the y position of a peak in a cell
+            dx - the difference between the x position of the peak in this cell
+                 and the x position of the same peak in the average cell
+            dy - the difference between the y position of the peak in this cell
+                 and the y position of the same peak in the average cell
+            h - the height of the peak
+            o - if a peak is not perfectly symmetric, the orientation of the peak.
+            e - the eccentricity of a peak.  i.e. how egg-shaped is it?
+            
+        indices - peak indices (integers) to select from.  Use this if you want to compare
+            only a few peaks in the cell structure to compare.
+            None selects all peaks.
+        """
+        if len(chars) > 0:
+            if len(indices) is 0:
+                indices = range(self.chest.root.cell_peaks.number_of_peaks)
+            # the columns we get are the combination of the chars with the
+            #   indices we want.
+            cols = [['%s%i' % (c, i) for i in indices] for c in chars]
+        else:
+            chars = ['dx', 'dy', 'h', 'o', 'e']
+            if len(indices) is 0:
+                indices = range(self.chest.getNodeAttr('/cell_peaks','number_of_peaks'))
+            # the columns we get are the combination of the chars with the
+            #   indices we want.
+            cols = [['%s%i' % (c, i) for c in chars] for i in indices]
+        # make the cols a simple list, rather than a list of lists
+        cols = [item for sublist in cols for item in sublist]
+        # get the data from the table
+        peak_data = self.chest.root.cell_peaks[:]
+        # return an ndarray with only the selected columns
+        return np.array(peak_data[cols]).view(float).reshape(len(cols), -1)
+
     def get_input_data(self):
         if self.on_peaks:
             # query the peak table for all fields EXCEPT the filename and file index
             # TODO: should we also exclude peak coordinates (keep only the shift?)
-            cols = self.chest.root.cell_peaks.colnames[2:]
-            data = self.chest.root.cell_peaks[:][cols]
-            data = data.view((np.float64, len(data.dtype.names)))
+            data = self.get_peak_data().T
             active_data_shape = data.shape
         else:
             active_data = self.parent.cell_controller.get_cell_set()
@@ -95,9 +139,32 @@ class MDAExecutionController(HasTraits):
             fs = self.chest.createTable('/mda_results/'+self.context, 'peak_factors',
                          description=table_description)
             self.chest.setNodeAttr('/mda_results/'+self.context, 'on_peaks', True)
-            for name_idx in xrange(len(names)):
-                # TODO: need to copy in data to table
-                pass
+            data = np.zeros((self.number_to_derive), dtype=factor_dtype)
+            row = fs.row
+            # record the factors
+            indices = range(self.chest.getNodeAttr('/cell_peaks','number_of_peaks'))
+            chars = ['dx', 'dy', 'h', 'o', 'e']
+            # the columns we get are the combination of the chars with the
+            #   indices we want.
+            cols = [['%s%i' % (c, i) for c in chars] for i in indices]            
+            # flatten that from a list of lists to a simple list
+            cols = [item for sublist in cols for item in sublist]            
+            for col in xrange(len(cols)):
+                data[cols[col]] = factors[:, col]
+            # record the x and y coordinates
+            coordinate_data_indices = [['%s%i' % (c, i) for c in ['x', 'y']] 
+                                        for i in indices]
+            # flatten that from a list of lists to a simple list
+            coordinate_data_indices = [item for sublist in coordinate_data_indices for item in sublist]
+            # get the row for the average cell from the cell_peaks table.
+            #   we use this for recording the X and Y coordinates of peaks,
+            #   which we do not feed into MDA itself.
+            peak_record = self.chest.root.cell_peaks.readWhere(
+                'filename == "average"')[0]        
+            for idx in coordinate_data_indices:
+                data[idx] = peak_record[idx]
+            fs.append(data)
+            fs.flush()
         else:
             score_table_title='image_scores'
             fs = self.chest.createCArray('/mda_results/'+self.context, 'image_factors',
@@ -118,9 +185,9 @@ class MDAExecutionController(HasTraits):
         ss = self.chest.createTable('/mda_results/'+self.context, score_table_title, 
                                     description=desc)
         # arrange data to populate the table
-        data = np.zeros((self.number_to_derive), dtype=dtypes)
-        data['filename']=self.chest.root.cell_description.col('filename')
-        data['file_idx']=self.chest.root.cell_description.col('file_idx')
+        data = np.zeros((self.chest.root.cell_peaks.nrows), dtype=dtypes)
+        data['filename']=self.chest.root.cell_peaks.col('filename')
+        data['file_idx']=self.chest.root.cell_peaks.col('file_idx')
         for col in xrange(self.number_to_derive):
             data[names[col]] = scores[:, col]
         # get the table and append the data to it
@@ -188,17 +255,13 @@ class MDAExecutionController(HasTraits):
         # we need to reshape the factors and scores to make sense.
         # for images, the factors are themselves images, while the scores are
         # line plots with one column per component.
-        if self.get_active_data_type() is "image":
+        if self.get_active_data_type() is "image" and not self.on_peaks:
             factors = factors.reshape((-1, datashape[-2], datashape[-1]))
-            factors.squeeze()
-            scores.reshape((datashape[0], -1))
         # for SIs, the factors are spectra, while the scores are images.
         elif ((self.get_active_data_type() is "spectrum") or
                 (self.get_active_data_type() is "peaks")):
-            factors = factors.reshape((datashape[0], -1))
             scores = scores.reshape((-1, datashape[-2], datashape[-1]))
-            scores.squeeze()
-        return factors, scores
+        return factors.squeeze(), scores.squeeze()
 
     def _create_new_context(self, MDA_type):
         import time

analyzarr/controllers/MDA_view.py
@@ -1,5 +1,6 @@
 from chaco.api import ArrayPlotData, BasePlotContainer, Plot
-from traits.api import Instance, Bool, Int, List, String, Array, on_trait_change
+from traits.api import Instance, Bool, Int, List, String, Array, \
+     Float, on_trait_change
 from BaseImage import BaseImageController
 import tables as tb
 # how much to compress the data
@@ -20,7 +21,13 @@ class MDAViewController(BaseImageController):
     _selected_peak = Int(0)
     contexts = Array()
     context = String('')
-    _selected_context = Int(0)
+    _selected_context = Int(-1)
+    dimensionality = Int(1)
+    _characteristics = List(["Height", "Orientation", "Eccentricity"])
+    _characteristic = Int(0)    
+    _show_shift = Bool(False)
+    shift_scale = Float(1.0)   
+    _can_map_peaks = Bool(False)
 
     def __init__(self, treasure_chest=None, data_path='/rawdata',
                  *args, **kw):
@@ -28,14 +35,22 @@ class MDAViewController(BaseImageController):
         self.factor_plotdata = ArrayPlotData()
         self.score_plotdata = ArrayPlotData()
         if treasure_chest is not None:
+            self.numfiles = len(treasure_chest.listNodes(data_path))
             self.chest = treasure_chest
             self.data_path = data_path
             # populate the list of available contexts (if any)
             if self.chest.root.mda_description.nrows>0:
                 self.contexts = self.chest.root.mda_description.col('context')
                 self.context = str(self.contexts[self._selected_context])
-                self.init_plots()
+                self.dimensionality = self.chest.getNodeAttr('/mda_results/'+self.context, 
+                                                             'dimensionality')
+                self.update_image()
     
+    def _selected_context_changed(self):
+        self.context = str(self.contexts[self._selected_context])
+        self.dimensionality = self.chest.getNodeAttr('/mda_results/'+context, 
+                                                     'dimensionality')
+        
     def increase_selected_component(self):
         # TODO: need to measure dimensionality somehow (node attribute, or array size?)
         if self.component_index == (self.dimensionality - 1):
@@ -49,58 +64,38 @@ class MDAViewController(BaseImageController):
         else:
             self.component_index -= 1    
 
-    # TODO: need to rethink how to set_data for these things, since we have so
-    #    many different places to put data.
-    def init_plots(self):
-        self.render_active_factor_image(self.context)
-        self.render_active_score_image(self.context)
-
     def render_active_factor_image(self, context):
         if self.chest.getNodeAttr('/mda_results/'+context, 'on_peaks'):
             factors = self.chest.getNode('/mda_results/'+context+'/peak_factors')
             # return average cell image (will be overlaid with peak info)
             self.factor_plotdata.set_data('imagedata', 
                                           self.chest.root.cells.average[:])
-            values = \
-                factors.read(start = self.component_index,
+            component = factors.read(start = self.component_index,
                              stop = self.component_index+1,
-                             step = 1,
-                             field='x_coordinate',) \
-                + \
-                factors.read(start = self.component_index,
-                             stop = self.component_index+1,
-                             step = 1,
-                             field='y%i'%self._selected_peak,)
-            
-            indices = \
-                factors.read(start = self.component_index,
-                             stop = self.component_index+1,
-                             step = 1,
-                             field='y_coordinate',) \
-                + \
-                factors.read(start = self.component_index,
-                             stop = self.component_index+1,
-                             step = 1,
-                             field='x%i'%self._selected_peak,)
-            self.plotdata.set_data('value', values)
-            self.plotdata.set_data('index', indices)
+                             step = 1,)[:]
+            numpeaks = self.chest.root.cell_peaks.getAttr('number_of_peaks')
+            index_keys = ['x%i' % i for i in xrange(numpeaks)]
+            value_keys = ['y%i' % i for i in xrange(numpeaks)]
+            color_prefix = self._characteristics[self._characteristic][0].lower()
+            color_keys = ['%s%i' % (color_prefix, i) for i in xrange(numpeaks)]
+            values = np.array(component[value_keys]).view(float)
+            indices = np.array(component[index_keys]).view(float)
+            color = np.array(component[color_keys]).view(float)
+            self.factor_plotdata.set_data('value', values)
+            self.factor_plotdata.set_data('index', indices)
+            self.factor_plotdata.set_data('color', color)
             
             if self._show_shift:
-                x_comp = factors.read(start = self.component_index,
-                                      stop = self.component_index+1,
-                                      step = 1,
-                                      field='dx%i'%self._selected_peak,
-                                      ).reshape((-1,1))
-                y_comp = factors.read(start = self.component_index,
-                                      stop = self.component_index+1,
-                                      step = 1,
-                                      field='dy%i'%self._selected_peak,
-                                      ).reshape((-1,1))
+                shift_x_keys = ['dx%i' % i for i in xrange(numpeaks)]
+                shift_y_keys = ['dy%i' % i for i in xrange(numpeaks)]
+                x_comp = np.array(component[shift_x_keys]).view(float).reshape((-1,1))
+                y_comp = np.array(component[shift_y_keys]).view(float).reshape((-1,1))
                 vectors = np.hstack((x_comp,y_comp))
                 vectors *= self.shift_scale
-                self.plotdata.set_data('vectors',vectors)            
+                self.factor_plotdata.set_data('vectors',vectors)            
             self.factor_plot = self.get_scatter_quiver_plot(self.factor_plotdata,
                                                           tool='inspector')
+            self._can_map_peaks=True
         else:
             factors = self.chest.getNode('/mda_results/'+context+'/image_factors')
             # return current factor image (MDA on images themselves)
@@ -119,15 +114,7 @@ class MDAViewController(BaseImageController):
                 'filename == "%s"' % self.get_active_name(),
                 field='y_coordinate',)
         if self.chest.getNodeAttr('/mda_results/'+context, 'on_peaks'):
-            scores = self.chest.getNode('/mda_results/'+context+'/peak_scores')
-            # if we're on peaks, then we can localize further, overlaying the
-            #   score right on top of the peak it applies to.
-            values += self.chest.root.cell_peaks.readWhere(
-                'filename == "%s"' % self.get_active_name(),
-                field='y%i'%self._selected_peak,)
-            indices += self.chest.root.cell_peaks.readWhere(
-                'filename == "%s"' % self.get_active_name(),
-                field='x%i'%self._selected_peak,)            
+            scores = self.chest.getNode('/mda_results/'+context+'/peak_scores')          
         else:
             scores = self.chest.getNode('/mda_results/'+context+'/image_scores')
         color = scores.readWhere(
@@ -137,16 +124,9 @@ class MDAViewController(BaseImageController):
         self.score_plotdata.set_data('index', values)
         self.score_plotdata.set_data('value', indices)
         self.score_plotdata.set_data('color', color)
-        self.get_scatter_overlay_plot(self.score_plotdata, tool="colorbar")
+        self.score_plot = self.get_scatter_overlay_plot(self.score_plotdata, tool="colorbar")
 
-    @on_trait_change("selected_index")
+    @on_trait_change("selected_index, component_index")
     def update_image(self):
-        self.render_active_factor_image()
-        self.render_active_score_image()
-        # TODO: customize this to change the factor data and plot data
-        self.factor_plotdata.set_data("imagedata", self.get_active_factor_image())
-        self.set_factor_plot_title("Factor %s of %s: " % (self.selected_index + 1,
-                                          self.numfactors) + self.get_active_name())
-        self.score_plotdata.set_data("imagedata", self.get_active_score_image())
-        self.set_score_plot_title("Score %s of %s: " % (self.selected_index + 1,
-                                          self.numfactors) + self.get_active_name())
+        self.render_active_factor_image(self.context)
+        self.render_active_score_image(self.context)